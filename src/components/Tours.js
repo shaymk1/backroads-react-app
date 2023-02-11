b14083eb53d49b80e9dfe37d015fb070ce5a03b0
unknown <mkekae@gmail.com>
2023-02-11 12:42:54 +0200

    mapping through the tour section

---
+++ src/components/Tours.js
@@ -0,0 +1,45 @@
+import { tours } from "../data";
+
+import Title from "./Title";
+
+const Tours = () => {
+  return (
+		<section className="section" id="tours">
+			<Title title="featured" subTitle="tours" />
+			<div className="section-center featured-center">
+				{
+					tours.map((tour)=>{
+						const {id,img,date,title,text,location,days,price} = tour
+						return (
+							<article className="tour-card" key = {id}>
+								<div className="tour-img-container">
+									<img src={img} className="tour-img" alt="" />
+									<p className="tour-date">{date}</p>
+								</div>
+								<div className="tour-info">
+									<div className="tour-title">
+										<h4>{title}</h4>
+									</div>
+									<p>
+										{text}
+									</p>
+									<div className="tour-footer">
+										<p>
+											<span>
+												<i className="fas fa-map"></i>
+											</span>{" "}
+											{location}
+										</p>
+										<p>{days} days</p>
+										<p>from {price}</p>
+									</div>
+								</div>
+							</article>
+						);
+					})
+				}
+			</div>
+		</section>
+	);
+}
+export default Tours
--- src/data.js
+++ src/data.js
@@ -1,3 +1,9 @@
+import tourImageOne from "./images/tour-1.jpeg";
+import tourImageTwo from "./images/tour-2.jpeg";
+import tourImageThree from "./images/tour-3.jpeg";
+import tourImageFour from "./images/tour-4.jpeg";
+
+// nav
 export const pageLinks = [
 	{
 		id: 1,
@@ -24,7 +30,6 @@ export const pageLinks = [
 	},
 ];
 
-
 export const socialLinks = [
 	{
 		id: 1,
@@ -42,5 +47,82 @@ export const socialLinks = [
 		id: 3,
 		href: "https://www.twitter.com",
 		icon: "fab fa-squarespace",
+	},
+];
+
+// serives
+
+export const services = [
+	{
+		id: 1,
+		icon: "fas fa-wallet fa-fw",
+		title: "Tibet Adventure",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+	},
+
+	{
+		id: 2,
+		icon: "fas fa-tree fa-fw",
+		title: "endless hiking",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+	},
+
+	{
+		id: 3,
+		icon: "fas fa-socks fa-fw",
+		title: "amazing comfort",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+	},
+];
+
+// tours
+
+export const tours = [
+	{
+		id: 1,
+		img: tourImageOne,
+		date: "august 26th, 2020",
+		title: "Tibet Adventure",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+		icon: "fas fa-map",
+		location: "china",
+		days: 6,
+		price: "$2100",
+	},
+
+	{
+		id: 2,
+		img: tourImageTwo,
+		date: "october 1th, 2020",
+		title: "explore hong kong",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+		icon: "fas fa-map",
+		location: "hong kong",
+		days: 8,
+		price: "$5000",
+	},
+
+	{
+		id: 3,
+		img: tourImageThree,
+		date: "october 1th, 2020",
+		title: "best of java",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+		icon: "fas fa-map",
+		location: "indonesia",
+		days: 11,
+		price: "$1400",
+	},
+
+	{
+		id: 4,
+		img: tourImageFour,
+		date: "december 5th, 2019",
+		title: "kenya highlights",
+		text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+		icon: "fas fa-map",
+		location: "kenya",
+		days: 20,
+		price: "$3300",
 	},
 ];
